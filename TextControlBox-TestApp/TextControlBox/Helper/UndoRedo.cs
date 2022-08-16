ba10cbae78b6037163c8d6974c9c8d76c115747a
Julius Kirsch <julius@skirsch.de>
2022-08-16 09:35:48 +0200

    Implemented list, but the ram usage is way to high

--- TextControlBox-TestApp/TextControlBox/Helper/UndoRedo.cs
+++ TextControlBox-TestApp/TextControlBox/Helper/UndoRedo.cs
@@ -1,18 +1,50 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 using TextControlBox_TestApp.TextControlBox.Renderer;
 
 namespace TextControlBox_TestApp.TextControlBox.Helper
 {
+    public class CustomStack<T>
+    {
+        List<T> Items = new List<T>();
+        int Limit = -1;
+
+        public T Pop()
+        {
+            if (Items.Count == 0)
+                return default;
+
+            T Item =  Items[Items.Count - 1];
+            Items.RemoveAt(Items.Count - 1);
+            return Item;
+        }
+        public void Push(T item)
+        {
+            //Remove the last item if limit is exceed
+            if (Items.Count > Limit && Limit != -1)
+                Items.RemoveAt(0);
+
+            Items.Add(item);
+        }
+        public void Clear()
+        {
+            Items.Clear();
+        }
+        public void SetLimit(int Limit)
+        {
+            this.Limit = Limit;
+        }
+        public int Count => Items.Count;
+    }
+
     public class UndoRedo
     {
         public string EnteringText { get; set; } = "";
-        private Stack<UndoRedoClass> UndoStack = new Stack<UndoRedoClass>();
-        private Stack<UndoRedoClass> RedoStack = new Stack<UndoRedoClass>();
-
+        private CustomStack<UndoRedoClass> UndoStack = new CustomStack<UndoRedoClass>();
+        
         public void ClearStacks()
         {
             UndoStack.Clear();
-            RedoStack.Clear();
         }
 
         public void RecordUndoOnPress(Line CurrentLine, CursorPosition CursorPosition)
