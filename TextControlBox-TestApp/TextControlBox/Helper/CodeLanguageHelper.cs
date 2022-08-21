9d2ed6b5f241d068036cd2c73f473d6555a435f1
Julius Kirsch <julius@skirsch.de>
2022-08-21 18:38:11 +0200

    This fixes #125

---
+++ TextControlBox-TestApp/TextControlBox/Helper/CodeLanguageHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextControlBox_TestApp.TextControlBox.Languages;
+
+namespace TextControlBox_TestApp.TextControlBox.Helper
+{
+    public class CodeLanguageHelper
+    {
+        public static CodeLanguage GetCodeLanguage(CodeLanguages Languages)
+        {
+            switch (Languages)
+            {
+                case CodeLanguages.Csharp:
+                    return new Csharp();
+                case CodeLanguages.Gcode:
+                    return new GCode();
+                case CodeLanguages.Html:
+                    return new Html();
+            }
+            return null;
+        }
+        public static CodeLanguages GetCodeLanguages(CodeLanguage Language)
+        {
+            if (System.Object.ReferenceEquals(Language, new Csharp()))
+                return CodeLanguages.Csharp;
+            else if (System.Object.ReferenceEquals(Language, new GCode()))
+                return CodeLanguages.Gcode;
+            else if (System.Object.ReferenceEquals(Language, new Html()))
+                return CodeLanguages.Html;
+            else return CodeLanguages.None;
+        }
+    }
+}
