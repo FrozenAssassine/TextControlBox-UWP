b5aec76516aaecfdde417830b1d6157cd9362e30
Julius K <julius@skirsch.de>
2022-12-24 14:47:03 +0100

    Update version

--- TextControlBox/Properties/AssemblyInfo.cs
+++ TextControlBox/Properties/AssemblyInfo.cs
@@ -23,6 +23,6 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden, 
 // indem Sie "*" wie unten gezeigt eingeben:
 // [Assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.3.1.0")]
+[assembly: AssemblyFileVersion("1.3.1.0")]
 [assembly: ComVisible(false)] 
